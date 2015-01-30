559d6027408f0d4fae598b5665704ca08abf7a09
Kevin Stubblefield <kevins12.92@gmail.com>
2015-01-29 22:39:45 -0600

    Added an introductory message.

---
+++ GitTest/src/gittest/GitTest.java
@@ -0,0 +1,11 @@
+
+package gittest;
+
+public class GitTest {
+
+    public static void main(String[] args) {
+        System.out.println("Hi! This is a test on how to use Git with Netbeans."
+                + "\nMaybe I should watch a video or something...");
+    }
+    
+}
